b051668ff82407ed4e2ac29e481a2c6036295652
vapaex <vapaex@gmail.com>
2013-05-14 23:52:23 +0200

    Viele (!) Änderungen und neue Features, Version 0.0.2

--- advancedhnh.user.js
+++ advancedhnh.user.js
@@ -6,8 +6,9 @@
 // @license      none
 // @include      http://tud.hicknhack.org/forum/*
 // @include      http://127.0.0.1/hnh/*
-// @version      0.0.1
-// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @grant        none
+// @version      0.0.2
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
 
 
@@ -54,6 +55,9 @@ var HNH_HIGHLIGHT_TODAY_YESTERDAY = true;
 // Seitennavigation nochmal unterhalb der Threadübersicht anzeigen
 var HNH_CLONE_PAGE_NAVIGATION = true;
 
+// besuchte Threads in Cookies speichern und hervorheben
+var HNH_ENABLE_COOKIE_FEATURES = true;
+
 // ---------- </THREADÜBERSICHT> ---------
 
 
@@ -67,9 +71,17 @@ var HNH_FIX_LINKS = true;
 // CSS-Regeln können weiter unten bearbeitet werden
 var HNH_HIGHLIGHT_PATTERNS = true;
 
-// Bilder anzeigen
-// ganz billige Implementierung
-var HNH_SHOW_IMAGES = false;
+// Links anonymisieren
+var HNH_ANONYMIZE_LINKS = true;
+
+// Präfix für anonymisierte Links
+var HNH_ANONYMOUS_LINK_PREFIX = 'http://dontknow.me/at/';
+
+// leere Überschrift-Divs verstecken
+var HNH_HIDE_EMPTY_HEADLINES = true;
+
+// Anker für jeden Beitrag hinzufügen
+var HNH_ADD_ANCHORS = true;
 
 // ---------- <BEITRAGSANSICHT> ----------
 
@@ -105,15 +117,6 @@ var HNH_SHORTCUTS = {
 
 // ---------- <CSS> ----------
 
-// CSS für "Heute"
-var HNH_CSS_TODAY = { 'color': '#000' };
-
-// CSS für "Gestern"
-var HNH_CSS_YESTERDAY = { 'color': '#333' };
-
-// CSS für weder "Heute" noch "Gestern"
-var HNH_CSS_OLDER = { 'color': '#555' };
-
 // Allgemeine CSS-Regeln
 // Greasemonkey vorausgesetzt
 var HNH_CSS_GLOBAL = '\
@@ -130,22 +133,22 @@ var HNH_CSS_GLOBAL = '\
 	} \
 	\
 	/* zitierter Text */ \
-	.body .quote, .body .quote a { \
+	.body .hnh_quote, .body .hnh_quote a { \
 		color: #666; \
 		font-style: italic; \
 	} \
 	\
 	/* [x] */ \
-	.body .check { \
+	.body .hnh_check { \
 		color: #070; \
 	} \
 	\
 	/* [ ] */ \
-	.body .nocheck { \
+	.body .hnh_nocheck { \
 		color: #a00; \
 	} \
 	\
-	.body .questioncheck { \
+	.body .hnh_questioncheck { \
 		color: #650; \
 	} \
 	\
@@ -159,10 +162,28 @@ var HNH_CSS_GLOBAL = '\
 		background-color: #eee; \
 		white-space: nowrap; \
 	} \
+	\
+	/* CSS für "Heute", "Gestern" und ältere Beiträge in der Threadübersicht */ \
+	#threads .topic a { font-weight: normal; } \
+	#threads .hnh_today { color: #000 !important; } \
+	#threads .hnh_yesterday { color: #333 !important; } \
+	#threads .hnh_older { color: #333 !important; font-weight: normal !important; } \
 ';
 
 
 // ---------- </CSS> ---------
+
+
+// ---------- <SONSTIGES> ----------
+
+// Cookie-Name
+var HNH_COOKIE_COUNT_NAME = 'hnh_count';
+
+// Trennsymbole für Serialisierung
+var HNH_SERIALIZE_SEPARATOR_1 = ':';
+var HNH_SERIALIZE_SEPARATOR_2 = ',';
+
+// ---------- </SONSTIGES> ---------
 
 
 // ------------------------------ </KONFIGURATION> -----------------------------
@@ -172,24 +193,11 @@ var HNH_CSS_GLOBAL = '\
 // ------------------------------ <FUNKTIONSAUFRUFE> ------------------------------
 
 
-(function() {
+$(document).ready(function() {
 	
 	hnhInit();
-	if (typeof HNH_HEADLINE !== 'undefined' && HNH_HEADLINE.length > 0) hnhChangeHeadline();
-	if (typeof HNH_HTML_TITLE !== 'undefined' && HNH_HTML_TITLE) hnhHtmlTitle();
-	if (typeof HNH_HIDE_SIDEBAR !== 'undefined' && HNH_HIDE_SIDEBAR) hnhHideSidebar();
-	if (typeof HNH_FORM_SPOILER !== 'undefined' && HNH_FORM_SPOILER) hnhFormSpoiler();
-	if (typeof HNH_CHANGE_NUMBER_FORMAT !== 'undefined' && HNH_CHANGE_NUMBER_FORMAT) hnhChangeNumberFormat();
-	if (typeof HNH_CLONE_PAGE_NAVIGATION !== 'undefined' && HNH_CLONE_PAGE_NAVIGATION) hnhClonePageNavigation();
-	if (typeof HNH_FIX_LINKS !== 'undefined' && HNH_FIX_LINKS) hnhFixLinks();
-	if (typeof HNH_HIGHLIGHT_TODAY_YESTERDAY !== 'undefined' && HNH_HIGHLIGHT_TODAY_YESTERDAY) hnhHighlightTodayYesterday();
-	if (typeof HNH_HIGHLIGHT_PATTERNS !== 'undefined' && HNH_HIGHLIGHT_PATTERNS) hnhHighlightPatterns();
-	if (typeof HNH_SHOW_IMAGES !== 'undefined' && HNH_SHOW_IMAGES) hnhShowImages();
-	if (typeof HNH_SHORTCUTS_SUBMIT !== 'undefined' && HNH_SHORTCUTS_SUBMIT) hnhRegisterShortcutsSubmit();
-	if (typeof HNH_SHORTCUTS_SCROLL !== 'undefined' && HNH_SHORTCUTS_SCROLL) hnhRegisterShortcutsScroll();
-	if (typeof HNH_SHORTCUTS_PHRASES !== 'undefined' && HNH_SHORTCUTS_PHRASES) hnhRegisterShortcutsPhrases();
 	
-})();
+});
 
 
 // ------------------------------ </FUNKTIONSAUFRUFE> -----------------------------
@@ -206,6 +214,24 @@ function hnhInit() {
 	
 	// globale CSS
 	GM_addStyle(HNH_CSS_GLOBAL);
+	
+	// Features
+	if (typeof HNH_HEADLINE !== 'undefined' && HNH_HEADLINE.length > 0) hnhChangeHeadline();
+	if (typeof HNH_HTML_TITLE !== 'undefined' && HNH_HTML_TITLE) hnhHtmlTitle();
+	if (typeof HNH_HIDE_SIDEBAR !== 'undefined' && HNH_HIDE_SIDEBAR) hnhHideSidebar();
+	if (typeof HNH_FORM_SPOILER !== 'undefined' && HNH_FORM_SPOILER) hnhFormSpoiler();
+	if (typeof HNH_CHANGE_NUMBER_FORMAT !== 'undefined' && HNH_CHANGE_NUMBER_FORMAT) hnhChangeNumberFormat();
+	if (typeof HNH_CLONE_PAGE_NAVIGATION !== 'undefined' && HNH_CLONE_PAGE_NAVIGATION) hnhClonePageNavigation();
+	if (typeof HNH_FIX_LINKS !== 'undefined' && HNH_FIX_LINKS) hnhFixLinks();
+	if (typeof HNH_HIGHLIGHT_TODAY_YESTERDAY !== 'undefined' && HNH_HIGHLIGHT_TODAY_YESTERDAY) hnhHighlightTodayYesterday();
+	if (typeof HNH_HIGHLIGHT_PATTERNS !== 'undefined' && HNH_HIGHLIGHT_PATTERNS) hnhHighlightPatterns();
+	if (typeof HNH_SHOW_IMAGES !== 'undefined' && HNH_SHOW_IMAGES) hnhShowImages();
+	if (typeof HNH_SHORTCUTS_SUBMIT !== 'undefined' && HNH_SHORTCUTS_SUBMIT) hnhRegisterShortcutsSubmit();
+	if (typeof HNH_SHORTCUTS_SCROLL !== 'undefined' && HNH_SHORTCUTS_SCROLL) hnhRegisterShortcutsScroll();
+	if (typeof HNH_SHORTCUTS_PHRASES !== 'undefined' && HNH_SHORTCUTS_PHRASES) hnhRegisterShortcutsPhrases();
+	if (typeof HNH_ENABLE_COOKIE_FEATURES !== 'undefined' && HNH_ENABLE_COOKIE_FEATURES) hnhCookieFeatures();
+	if (typeof HNH_HIDE_EMPTY_HEADLINES !== 'undefined' && HNH_HIDE_EMPTY_HEADLINES) hnhHideEmptyHeadlines();
+	if (typeof HNH_ADD_ANCHORS !== 'undefined' && HNH_ADD_ANCHORS) hnhAddAnchors()
 }
 
 
@@ -228,13 +254,25 @@ function hnhHtmlTitle() {
 // Versteckt die rechte Seitenleiste, die das Login-Formular beinhaltet
 function hnhHideSidebar() {
 	$('table.site td.sidebar').hide();
+	
+	$('td.title div.navigation').append('<div class="showhidesidebar"></div><div clear="both"></div>\n');
+	
+	var tlink = document.createElement('a');
+	$(tlink).attr('href', 'javascript:void(0)').html('+').click(function() {
+		$('table.site td.sidebar').toggle();
+	});
+	
+	$('.showhidesidebar').append(tlink);
+	
+	// CSS
+	$('.showhidesidebar').css('float', 'right');
 }
 
 
 // Packt das Formular in einen Spoiler
 function hnhFormSpoiler() {
 	var form = $('form#new_forum_message');
-	if (HNH_HIDE_FORM && $('textarea#forum_message_body').text().length == 0) form.hide();
+	if (typeof HNH_HIDE_FORM !== 'undefined' && HNH_HIDE_FORM && $('textarea#forum_message_body').text().length == 0) form.hide();
 	
 	form.before('<div id="togglediv"><a href="javascript:void(0);" id="togglelink">Formular anzeigen/ausblenden</a></div>\n');
 	$('#togglelink').click(function() {
@@ -256,8 +294,11 @@ function hnhFormSpoiler() {
 
 // Ändert das Format der Beitragszahl in der Threadliste
 function hnhChangeNumberFormat() {
-	// Kopfzeile verbreitern
-	$('div#threads table.foren thead th:nth-child(2)').attr('colspan', 2);
+	// Kopf- und Fußzeile verbreitern
+	var oHead = $('div#threads table.foren thead th:nth-child(2)');
+	var oFoot = $('div#threads table.foren tfoot td:nth-child(1)');
+	oHead.attr('colspan', oHead.attr('colspan') > 0 ? oHead.attr('colspan') + 1 : 2);
+	oFoot.attr('colspan', oFoot.attr('colspan') > 0 ? oFoot.attr('colspan') + 1 : 2);
 	
 	$('div#threads table.foren tbody td:nth-child(2)').each(function(index) {
 		var text = $(this).html();
@@ -274,9 +315,6 @@ function hnhChangeNumberFormat() {
 		$(this).after('<td>' + td2 + '&nbsp;</td>');
 	});
 	$('div#threads table.foren tbody td:nth-child(3)').css('border-left', '1px solid #000');
-	
-	// Fußzeile verbreitern
-	$('div#threads table.foren tfoot td:nth-child(1)').attr('colspan', 4);
 }
 
 
@@ -288,11 +326,11 @@ function hnhHighlightTodayYesterday() {
 		var text = $(this).html();
 		
 		if (text.match(/^Heute/))
-			$(this).parent().find('td, a').css(HNH_CSS_TODAY);
+			$(this).parent().find('td, td a').addClass('hnh_today');
 		else if (text.match(/^Gestern/))
-			$(this).parent().find('td, a').css(HNH_CSS_YESTERDAY);
+			$(this).parent().find('td, td a').addClass('hnh_yesterday');
 		else
-			$(this).parent().find('td, a').css(HNH_CSS_OLDER);
+			$(this).parent().find('td, td a').addClass('hnh_older');
 	});
 }
 
@@ -308,7 +346,7 @@ function hnhFixLinks() {
 	$('table.foren tbody tr.message td.text div.body a').each(function(index) {
 		var href = $(this).attr('href');
 		var text = $(this).html();
-		var tld = href.replace(/^(https?|ftp):\/\/(.*\.)*(.*\..*)\/.*$/, '$3');
+		var tld = href.replace(/^(https?|ftp):\/\/(.*\.)*(.*?\..*?)\/.*$/, '$3');
 		
 		// "&" korrigieren
 		href = href.replace(/&(amp;)+/g, '\&');
@@ -319,6 +357,8 @@ function hnhFixLinks() {
 		var arrDesc = text.match(/^(.*?)\s\-\s(.*)$/);
 		var arrInt = href.match(/^(http):\/\/tud\.hicknhack\.org\/forum\/messages\/([0-9]+)$/);
 		
+		// TODO andere interne Links (tud.hicknhack.org/.*) erkennen und auswerten
+		
 		if (arrDesc) mode++;
 		if (arrInt) mode += 2;
 		
@@ -328,16 +368,21 @@ function hnhFixLinks() {
 				break;
 			
 			case 1: // externer Link mit Beschreibung
-				text = tld + ' - ' + arrDesc[2];
+				text = arrDesc[2] + ' (' + tld + ')';
 				break;
 			
 			case 2: // interner Link ohne Beschreibung
-				text = 'HnH #' + arrInt[2];
+				text = 'Thread ' + arrInt[2] + ' (HnH)';
 				break;
 			
 			case 3: // interner Link mit Beschreibung
-				text = 'HnH - ' + arrDesc[2];
+				text = arrDesc[2] + ' (HnH)';
 				break;
+		}
+		
+		if (mode < 2 && typeof(HNH_ANONYMIZE_LINKS !== 'undefined') && HNH_ANONYMIZE_LINKS) {
+			// externen Link anonymisieren
+			href = HNH_ANONYMOUS_LINK_PREFIX + href;
 		}
 		
 		$(this).attr('href', href);
@@ -352,10 +397,10 @@ function hnhHighlightPatterns() {
 	$('table.foren tbody tr.message td.text div.body').each(function(index) {
 		var result = correctBreaks($(this).html());
 		
-		result = result.replace(/^(&gt;.*$)/gm, '<span class="quote">$1</span>');
-		result = result.replace(/^(\[[xX]\].*)$/gm, '<span class="check">$1</span>');
-		result = result.replace(/^(\[\s+\].*)$/gm, '<span class="nocheck">$1</span>');
-		result = result.replace(/^(\[[\?]\].*)$/gm, '<span class="questioncheck">$1</span>');
+		result = result.replace(/^(&gt;.*$)/gm, '<span class="hnh_quote">$1</span>');
+		result = result.replace(/^(\[[xX]\].*)$/gm, '<span class="hnh_check">$1</span>');
+		result = result.replace(/^(\[\s+\].*)$/gm, '<span class="hnh_nocheck">$1</span>');
+		result = result.replace(/^(\[[\?]\].*)$/gm, '<span class="hnh_questioncheck">$1</span>');
 		
 		$(this).html(result);
 	});
@@ -430,6 +475,135 @@ function hnhRegisterShortcutsPhrases() {
 }
 
 
+// Speichert gelesene Threads im Cookie und markiert sie entsprechend in der Threadübersicht
+function hnhCookieFeatures() {
+	// Cookies lesen
+	var cookieCountVal = getCookie(HNH_COOKIE_COUNT_NAME);
+	var countArr = new Object();
+	if (cookieCountVal) countArr = hnhDeserialize(cookieCountVal);
+	
+	// JS-Objekt der Überschrift
+	var to = $('td.title div.title');
+	
+	// Kopfzeile erweitern und Fußzeile verbreitern
+	if (typeof HNH_SHOW_OPTIONS_COLUMN !== 'undefined' && HNH_SHOW_OPTIONS_COLUMN) {
+		$('div#threads table.foren thead th:last-child').after('<th>Optionen</th>');
+		$('div#threads table.foren thead th:last-child').css('border-left', '1px solid #000');
+		var oFoot = $('div#threads table.foren tfoot td:nth-child(1)');
+		oFoot.attr('colspan', oFoot.attr('colspan') > 0 ? oFoot.attr('colspan') + 1 : 2);
+	}
+	
+	if ($('td.content table.foren tr.message').length > 0) {
+		
+		// Thread-Detailansicht
+		
+		var now = Math.round(new Date().getTime() / 1000);
+		
+		// Thread-ID und Anzahl der Beiträge holen
+		var threadId = $('#forum_message_thread_id').val();
+		var postCount = $('tr.message').length;
+		
+		countArr[threadId] = postCount;
+		
+		// TODO Wenn man gerade einen neuen Beitrag gepostet hat,
+		// dann hat man ihn natürlich auch schon gelesen
+		// "Voransicht"-Divs dürfen nicht berücksichtigt werden
+		
+	}
+	else {
+		
+		// Thread-Übersicht
+		
+		// Anzahl der beobachteten Threads mit ungelesenen Beiträgen
+		var unreadCount = 0;
+		
+		$('div#threads table.foren tbody tr').each(function(index) {
+			var href = $(this).find('div.topic a').attr('href');
+			var threadId = href.substring(16, href.length);
+			var postCount = parseInt($(this).find('td:nth-child(3)').text());
+			
+			var readCount = 0;
+			//var cookieCountName = 'hnh_count_' + threadId;
+			//var cookieCountVal = getCookie(cookieCountName);
+			//if (cookieCountVal) readCount = parseInt(cookieCountVal);
+			
+			if (countArr[threadId] != null) {
+				readCount = countArr[threadId];
+			}
+			
+			// Spalte mit Optionen hinzufügen
+			if (typeof HNH_SHOW_OPTIONS_COLUMN !== 'undefined' && HNH_SHOW_OPTIONS_COLUMN) {
+				$(this).find('td:last-child').after('<td><i>(to do)</i></td>');
+				$(this).find('td:last-child').css('border-left', '1px solid #000');
+			}
+			
+			if (readCount > 0) {
+				// der Thread wurde schon mal gelesen
+				
+				$(this).find('div.topic').prepend('[x] ');
+				
+				if (readCount < postCount) {
+					// einige Beiträge im Thread wurden noch nicht gelesen
+					
+					$(this).find('div.topic').append(' [' + (postCount - readCount) + ']');
+					$(this).find('td a').attr('href', $(this).find('td a').attr('href') + '#hnh' + (readCount + 1));
+					$(this).find('td a').css({ 'font-weight': 'bold' });
+					
+					unreadCount++;
+				}
+				else {
+					// der Thread wurde komplett gelesen
+					
+					//$(this).find('td, td a').css({ 'color': '#333', 'font-weight': 'normal' });
+					$(this).find('td a').attr('href', $(this).find('td a').attr('href') + '#bottom');
+				}
+			}
+			else {
+				// der Thread wurde noch nicht gelesen
+				
+				$(this).find('div.topic').prepend('[ ] ');
+			}
+		});
+		
+		// Anzahl der beobachteten Threads mit ungelesenen Beiträgen im HTML-Titel anzeigen
+		if (unreadCount > 0) $('title').html('(' + unreadCount + ') ' + $('title').html());
+		
+	}
+	
+	// Cookies schreiben
+	setCookie(HNH_COOKIE_COUNT_NAME, hnhSerialize(countArr), null, null, '/');
+}
+
+
+// Versteckt leere head-Divs in Threads
+function hnhHideEmptyHeadlines() {
+	$('.content .message .head').each(function() {
+		if ($(this).text().length < 1)
+			$(this).hide();
+	});
+}
+
+
+// Fügt jedem Beitrag einen Anker hinzu
+// ACHTUNG: kann z. B. im "Schöne Frauen"-Thread sehr lange dauern
+function hnhAddAnchors() {
+	// Anker für jeden Artikel
+	var i = 1;
+	$('tr.message').each(function() {
+		// Voransicht eines neuen Beitrags wird nicht berücksichtigt
+		if ($(this).attr('id') != 'msg') {
+			$(this).find('td:first').attr('id', 'hnh' + i);  // tr hat schon eine ID
+			$(this).find('div.date').prepend('<a href="#hnh' + i + '">#' + i + '</a>\n');
+			i++;
+		}
+	});
+	
+	// an entsprechende Stelle scrollen, falls Anker-Link angegeben
+	var hash = window.location.hash
+	if (hash.length > 0) if ($(hash).length > 0) $('html, body').scrollTop($(hash).offset().top);
+}
+
+
 // ------------------------------ </FEATURE-FUNKTIONEN> -----------------------------
 
 
@@ -441,12 +615,8 @@ function hnhRegisterShortcutsPhrases() {
 function taAppendText(s) {
 	var ta = $('textarea#forum_message_body');
 	
-	if (ta.html().length > 0) {
-		ta.html(ta.html().replace(/\n$/, '')).append('\n' + s);
-	}
-	else {
-		ta.html(s);
-	}
+	if (ta.html().length > 0) ta.html(ta.html().replace(/\n$/, '')).append('\n' + s);
+	else ta.html(s);
 	
 	ta.focus();
 	scrollToBottom(ta);
@@ -461,6 +631,71 @@ function scrollToBottom(obj) {
 // Optimiert Zeilenumbrüche
 function correctBreaks(s) {
 	return s.replace(/[\r\n]*<br.*?>[\r\n]*/g, '<br />\n');
+}
+
+
+// Setzt einen Cookie
+function setCookie(name, value, domain, expires, path, secure) {
+	var cook = name + "=" + unescape(value);
+	cook += (domain) ? "; domain=" + domain : "";
+	cook += (expires) ? "; expires=" + expires : "";
+	cook += (path) ? "; path=" + path : "";
+	cook += (secure) ? "; secure" : "";
+	document.cookie = cook;
+}
+ 
+
+// Liest einen Cookie aus
+function getCookie(name) {
+    var i = 0;
+    var suche = name + "=";
+    while (i < document.cookie.length) {
+        if (document.cookie.substring(i, i + suche.length) == suche) {
+            var ende = document.cookie.indexOf(";", i + suche.length);
+            ende = (ende > -1) ? ende : document.cookie.length;
+            var cook = document.cookie.substring(i + suche.length, ende);
+            return unescape(cook);
+        }
+        i++;
+    }
+    return null;
+}
+
+
+// Löscht einen Cookie
+function deleteCookie(name, domain, path) {
+    var cook = name + "=; expires=Thu, 01-Jan-70 00:00:01 GMT";
+    cook += (domain) ? "domain=" + domain : "";
+    cook += (path) ? "path=" + path : "";
+    document.cookie = cook;
+}
+
+
+// Serialisierung eines assoziativen Arrays
+function hnhSerialize(arr) {
+	var res = new String();
+	for (var key in arr) {
+		res += key + HNH_SERIALIZE_SEPARATOR_1 + arr[key] + HNH_SERIALIZE_SEPARATOR_2;
+	}
+	
+	// letztes Trennzeichen abschneiden
+	res = res.substr(0, res.length - HNH_SERIALIZE_SEPARATOR_2.length);
+	
+	return res;
+}
+
+
+// Deserialisierung eines assoziativen Arrays (int)
+function hnhDeserialize(str) {
+	var res = new Object();
+
+	var arr = str.split(HNH_SERIALIZE_SEPARATOR_2);
+	for (var key in arr) {
+		var arr2 = arr[key].split(HNH_SERIALIZE_SEPARATOR_1);
+		res[arr2[0]] = parseInt(arr2[1]);
+	}
+
+	return res;
 }
 
 
